d1d919204ba98088def3e7d0406e02b5a4093121
Emask7 <emask7@gmail.com>
2025-04-28 14:14:30 -0500

    added more TLR9 pathway heatmaps

--- functional_analyses/pathway_heatmaps.R
+++ functional_analyses/pathway_heatmaps.R
@@ -23,11 +23,21 @@ colnames(annotation_df) <- c("Treatment", "Experiment")
 rownames(annotation_df) <- sample_list
 annotation_df  
 
+annotation_df_AM14only <- read.csv("raw_data/sample_info.csv")
+annotation_df_AM14only <- data.frame(annotation_df_AM14only$Treatment)
+annotation_df_AM14only <- data.frame(annotation_df_AM14only[1:20, ])
+colnames(annotation_df_AM14only) <- c("Treatment")
+rownames(annotation_df_AM14only) <- sample_list[1:20]
+annotation_df_AM14only
+
 ann_colors4 <- list(Experiment = c("AM14 Transfer + PL2-3 Stimulation" = "#00496f", 
                                    "AM14 Transfer + R848 Stimulation" = "#0f85a0", 
                                    "B1-8 Transfer + NP Stimulation" = "#edd746", 
                                    "AM14 MRL/lpr" = "#dd4124"),
                     Treatment = c("2DG" = "darkgray", "Control" = "beige"))
+
+ann_colors1 <- list(Treatment = c(PL23 = "#00496f", PL23_2DG = "#0f85a0", R848 = "#edd746", R848_2DG = "#dd4124"))
+
 
 GO_TLR9 <- join_all(list(
   get_pathway_genes("pathway_gene_lists/GOBP_TOLL_LIKE_RECEPTOR_9_SIGNALING_PATHWAY.txt", dds_AM14trans, gene_IDs$AM14trans[, 1:2]),
@@ -40,6 +50,15 @@ GO_TLR9 <- GO_TLR9[, c(2:ncol(GO_TLR9))]
 GO_TLR9 <- as.matrix(GO_TLR9)
 GO_TLR9 <- na.omit(GO_TLR9)
 
+
+
+GO_TLR9_AM14only <- get_pathway_genes("pathway_gene_lists/GOBP_TOLL_LIKE_RECEPTOR_9_SIGNALING_PATHWAY.txt", dds_AM14trans, gene_IDs$AM14trans[, 1:2])
+GO_TLR9_AM14only <- GO_TLR9_AM14only[, c(2:ncol(GO_TLR9_AM14only))]
+GO_TLR9_AM14only <- as.matrix(GO_TLR9_AM14only)
+GO_TLR9_AM14only <- na.omit(GO_TLR9_AM14only)
+GO_TLR9_AM14only
+
+
 png(filename = "Output/Functional_analyses/enrichGO_plots/TLR9 Signaling Pathway.png",
     width = 1200, height = 750, units = "px", pointsize = 10, res = 150,
     bg = "white", family = "", symbolfamily="default")
@@ -51,5 +70,17 @@ ComplexHeatmap::pheatmap(GO_TLR9,
                          heatmap_legend_param = list(title = "Z-score"))
 dev.off()
 
+
+
+png(filename = "Output/Functional_analyses/enrichGO_plots/TLR9 Signaling Pathway - AM14 transfer only.png",
+    width = 1000, height = 750, units = "px", pointsize = 10, res = 150,
+    bg = "white", family = "", symbolfamily="default")
+ComplexHeatmap::pheatmap(GO_TLR9_AM14only, 
+                         annotation_col = annotation_df_AM14only, scale = "row", 
+                         cluster_cols = FALSE, show_colnames = FALSE,
+                         annotation_colors = ann_colors1,
+                         main = "TLR9 Signaling Pathway\n(AM14 Adoptive Transfer Experiment Only)",
+                         heatmap_legend_param = list(title = "Z-score"))
+dev.off()
 
 
