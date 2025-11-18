12d50cb9c8d74aa89edba9d21dd2d970fe692619
Emask7 <emask7@gmail.com>
2025-11-18 15:28:33 -0600

    removed outlier (AM14 MRLlpr 2DG mouse 9) from AM14 MRLlpr analysis

---
+++ AM14_MRLlpr_no_outlier/DESeq2_MRLlpr_no_outlier.R
@@ -0,0 +1,121 @@
+setwd("Z:/Emma Mask/R_projects/AM14_with_B1-8_comparison/")
+getwd()
+
+# Import raw counts from Rosalind output ---------------------------------------
+  cts_AM14MRLlpr_noout <- import_Rosalind_data("raw_data/AM14_MRLlpr_rawCountsWithAnnotations.txt", ipa_format = TRUE)
+
+  # Save a table of gene IDs in case they need to be converted later -----------
+    gene_IDs <-  cts_AM14MRLlpr_noout[, c(1:6)]
+    head(gene_IDs)
+        
+  # Remove extra columns and outlier from cts ----------------------------------
+    cts_AM14MRLlpr_noout <- cts_AM14MRLlpr_noout[, c(9:14, 17:22)]
+    head(cts_AM14MRLlpr_noout)
+
+# Set up experimental factors --------------------------------------------------
+  coldata <- read.csv("raw_data/sample_info.csv")
+  coldata <- coldata[, c(1:3, 5:10)]
+  coldata
+  
+  cd_AM14MRLlpr_noout <- filter(coldata, Cohort == "M2")
+  cd_AM14MRLlpr_noout <- filter(cd_AM14MRLlpr_noout, Sample != "MRL_2DG_9")
+  cd_AM14MRLlpr_noout$Cohort <- factor(cd_AM14MRLlpr_noout$Cohort)
+  cd_AM14MRLlpr_noout$Treatment <- factor(cd_AM14MRLlpr_noout$Treatment)
+  cd_AM14MRLlpr_noout
+
+  # Make sure the columns of cts and rows of coldata are in the same order -----
+    for (x in 1:ncol(cts_AM14MRLlpr_noout)) {
+      if(colnames(cts_AM14MRLlpr_noout)[x] == cd_AM14MRLlpr_noout$Sample[x]) print(c(x, "true"))
+      else print(c(x, "false"))
+    }
+    rm(x)
+
+# Set up DESeq Data Set --------------------------------------------------------
+  dds_AM14MRLlpr_noout <- DESeqDataSetFromMatrix(countData = cts_AM14MRLlpr_noout,
+                                                 colData = cd_AM14MRLlpr_noout,
+                                                 design = ~Treatment)
+  dds_AM14MRLlpr_noout
+  keep <- rowSums(counts(dds_AM14MRLlpr_noout) >= 10) >= 6
+  dds_AM14MRLlpr_noout <- dds_AM14MRLlpr_noout[keep,]
+  dds_AM14MRLlpr_noout <- DESeq(dds_AM14MRLlpr_noout)
+  resultsNames(dds_AM14MRLlpr_noout)
+  rm(keep)
+  
+    
+# Run QC steps -----------------------------------------------------------------
+  setwd("./AM14_MRLlpr_no_outlier/")
+  getwd()
+
+  QC_heatmaps(dds_AM14MRLlpr_noout, "AM14_MRLlpr", "AM14 MRL/lpr mice\n2DG vs Control", plot_cohorts = FALSE)
+
+  sample_to_sample_plot(dds_AM14MRLlpr_noout, "AM14_MRLlpr", "AM14 MRL/lpr +/- 2DG")
+
+  QC_PCAplot(dds_AM14MRLlpr_noout, "AM14_MRLlpr", "AM14 MRL/lpr mice: 2DG vs Control",
+             batch_effect = NULL, draw_ellipse = FALSE)
+  dev.off()
+
+# Get DESeq2 results -----------------------------------------------------------
+  deseq_res_MRLlpr_noout <- results_wrapper(dds_AM14MRLlpr_noout, c("Treatment", "2DG", "Control"), gene_IDs)
+  head(deseq_res_MRLlpr_noout)
+  
+# DEG Heatmaps using default DESeq2 normalization ------------------------------
+  DEG_heatmap(dds_AM14MRLlpr_noout, deseq_res_MRLlpr_noout,
+              c("Treatment"),
+              list(Treatment = c(Control = "#0f85a0", "2DG" = "#dd4124")),
+              1, 0.05, "norm_counts", NULL,
+              "AM14 MRL/lpr Mice: 2DG vs Control",
+              "AM14 MRLlpr - padj 0.05")
+  
+  # DEG_heatmap(dds_AM14MRLlpr_noout, deseq_res_MRLlpr_noout,
+  #             c("Treatment"),
+  #             list(Treatment = c(Control = "#0f85a0", "2DG" = "#dd4124")),
+  #             1, 0.01, "norm_counts", NULL,
+  #             "AM14 MRL/lpr Mice: 2DG vs Control",
+  #             "AM14 MRLlpr - padj 0.01")
+  
+# # DEG Heatmaps using Rlog normalization ----------------------------------------  
+#   DEG_heatmap(dds_AM14MRLlpr_noout, deseq_res_MRLlpr_noout, 
+#               c("Treatment"),
+#               list(Treatment = c(Control = "#0f85a0", "2DG" = "#dd4124")),
+#               1, 0.05, "rlog", NULL,
+#               "AM14 MRL/lpr Mice: 2DG vs Control",
+#               "AM14 MRLlpr - padj 0.05 - RLog Normalization")
+#   
+#   DEG_heatmap(dds_AM14MRLlpr_noout, deseq_res_MRLlpr_noout, 
+#               c("Treatment"),
+#               list(Treatment = c(Control = "#0f85a0", "2DG" = "#dd4124")),
+#               1, 0.01, "rlog", NULL,
+#               "AM14 MRL/lpr Mice: 2DG vs Control",
+#               "AM14 MRLlpr - padj 0.01 - RLog Normalization")
+  
+  
+  
+
+# Make a data.frame that summarizes the numbers of DEGs detected -------------
+  full_summary_noout <- join_all(list(summary_wrapper(deseq_res_MRLlpr_noout, "2DG vs Control", "AM14 MRL/lpr: Cohort 2", 0.05, 1),
+                                summary_wrapper(deseq_res_MRLlpr_noout, "2DG vs Control", "AM14 MRL/lpr: Cohort 2", 0.05, 0.6)),
+                           type = "full")
+  full_summary_noout
+
+# Save results to an Excel file ----------------------------------------------
+  # setwd("Z:/Emma Mask/R_projects/AM14_with_B1-8_comparison/AM14_MRLlpr_no_outlier/")
+  # getwd()
+
+  wb <- createWorkbook("Output/DESeq2_results - AM14 MRLlpr no outlier.xlsx")
+
+  addWorksheet(wb, "DEG Summaries")
+  writeData(wb, "DEG Summaries", full_summary_noout)
+
+  addWorksheet(wb, "AM14 MRLlpr - no outlier")
+  writeData(wb, "AM14 MRLlpr - no outlier", deseq_res_MRLlpr_noout)
+
+  saveWorkbook(wb, "Output/DESeq2_results - AM14 MRLlpr no outlier.xlsx", overwrite = TRUE)
+  rm(wb)
+
+  wb <- createWorkbook("Output/DESeq2_normalized_gene_counts - AM14 MRLlpr no outlier.xlsx")
+
+  addWorksheet(wb, "MRLlpr")
+  writeData(wb, "MRLlpr", counts(dds_AM14MRLlpr_noout, normalized = TRUE), rowNames = TRUE)
+
+  saveWorkbook(wb, "Output/DESeq2_normalized_gene_counts - AM14 MRLlpr no outlier.xlsx", overwrite = TRUE)
+  rm(wb)
---
+++ AM14_MRLlpr_no_outlier/data_visualization.R
@@ -0,0 +1,93 @@
+setwd("./AM14_MRLlpr_no_outlier/")
+getwd()
+
+# DEG venn diagram -------------------------------------------------------------
+  DEG_lists_noout <- list(
+    PL23_2DG_vs_Ctrl = deg_list_for_venn(deseq_res$AM14transfer_PL23, 0.6, 0.05),
+    R848_2DG_vs_Ctrl = deg_list_for_venn(deseq_res$AM14transfer_R848, 0.6, 0.05),
+    B18 = deg_list_for_venn(deseq_res$B18transfer, 0.6, 0.05),
+    MRLlpr = deg_list_for_venn(deseq_res_MRLlpr_noout, 0.6, 0.05)
+  )
+  
+  all_degs <- venndetail(list("AM14 Transfer: PL2-3 + 2DG vs PL2-3" = DEG_lists_noout$PL23_2DG_vs_Ctrl$all,
+                              # "AM14 Transfer: R848 + 2DG vs R848" = DEG_lists_noout$R848_2DG_vs_Ctrl$all,
+                              # "B1-8 Transfer: NP + 2DG vs NP" = DEG_lists_noout$B18$all,
+                              "AM14 MRL/lpr: 2DG vs Control" = DEG_lists_noout$MRLlpr$all))
+  png(filename = "Output/Venn_diagrams/All_DEGs.png", width = 2000, height = 1500, res = 250)
+  plot(all_degs, type = "upset")
+  dev.off()
+
+# DEG overlap heatmap ----------------------------------------------------------
+  pl23_DEG_df <- deseq_res$AM14transfer_PL23[, c(1, 2, 8, 11, 12)]
+  pl23_DEG_df <- filter(pl23_DEG_df, abs(log2FoldChange) > 0.6 & padj <= 0.05)
+  nrow(pl23_DEG_df)
+  head(pl23_DEG_df)
+  
+  MRLlpr_DEG_df_noout <- deseq_res_MRLlpr_noout[, c(1, 2, 8, 11, 12)]
+  MRLlpr_DEG_df_noout <- filter(MRLlpr_DEG_df_noout, abs(log2FoldChange) > 0.6 & padj <= 0.05)
+  nrow(MRLlpr_DEG_df_noout)
+  head(MRLlpr_DEG_df_noout)
+  
+  merged_DEG_df_noout <- pl23_DEG_df
+  colnames(merged_DEG_df_noout)  <- c("ensembl_gene_id", "external_gene_name", "PL23_LFC", "PL23_pvalue", "PL23_padj")
+  merged_DEG_df_noout <- inner_join(merged_DEG_df_noout, MRLlpr_DEG_df_noout)
+  colnames(merged_DEG_df_noout)  <- c("ensembl_gene_id", "external_gene_name", "PL23_LFC", "PL23_pvalue", "PL23_padj", "MRLlpr_LFC", "MRLlpr_pvalue", "MRLlpr_padj")
+  head(merged_DEG_df_noout)
+  nrow(merged_DEG_df_noout)
+  
+  ID_conversion <- merged_DEG_df_noout[, 1:2]
+  ID_conversion
+  
+  DEGs <- merged_DEG_df_noout$ensembl_gene_id
+  head(DEGs)
+  
+  
+  zscore_matrix <- function(dds){
+    zscores <- counts(dds, normalized=TRUE)
+    res_colnames <- colnames(zscores)
+    zscores <- t(zscores)
+    zscores <- scale(zscores)
+    zscores <- t(zscores)
+    zscores <- data.frame(rownames(zscores), zscores)
+    zscores <- zscores[rownames(zscores) %in% DEGs, ]
+    colnames(zscores) <- c("ensembl_gene_id", res_colnames)
+    rm(res_colnames)
+    return(zscores)
+  }
+  
+  pl23_zscores <- zscore_matrix(dds_AM14trans_PL23)
+  head(pl23_zscores)
+  
+  MRLlpr_zscores_noout <- zscore_matrix(dds_AM14MRLlpr_noout)
+  head(MRLlpr_zscores_noout)
+  
+  DEG_zscores_noout <- inner_join(pl23_zscores, MRLlpr_zscores_noout)
+  DEG_zscores_noout <- inner_join(ID_conversion, DEG_zscores_noout)
+  rownames(DEG_zscores_noout) <- DEG_zscores_noout$external_gene_name
+  DEG_zscores_noout <- as.matrix(DEG_zscores_noout[, c(3:ncol(DEG_zscores_noout))])
+  head(DEG_zscores_noout)
+  DEG_zscores_noout
+  
+  coldata_df_noout <- data.frame(Treatment = c(rep("Control", 3), rep("2DG", 3), rep("Control", 6), rep("2DG", 6)),
+                                 Experiment = c(rep("AM14 + PL2-3", 6), rep("AM14 MRLlpr", 12)))
+  coldata_df_noout
+  
+  png(filename = ("Output/DEG_heatmaps/DEGs common in AM14 PL2-3 and AM14 MRLlpr - no outlier.png"),
+      width = 2000, height = 3500, units = "px", pointsize = 8, res = 400,
+      bg = "white", family = "", symbolfamily="default")
+  
+  htmp <- ComplexHeatmap::pheatmap(DEG_zscores_noout,
+                                   cluster_rows = TRUE, show_rownames = TRUE,
+                                   cluster_cols = TRUE, show_colnames = FALSE, 
+                                   annotation_col = coldata_df_noout, 
+                                   scale = "none",
+                                   cutree_rows = 3, 
+                                   annotation_colors = list(Experiment = c("AM14 + PL2-3" = "#0f85a0", "AM14 MRLlpr" = "#00496f"),
+                                                            Treatment = c(Control = "#edd746", "2DG" = "#dd4124")),
+                                   # main = "DEGs Common to AM14 Adoptive Transfer PL2-3 +/- 2DG and AM14 MRL/lpr +/- 2DG",
+                                   heatmap_legend_param = list(title = "Z-score", direction = "horizontal"))
+  
+  draw(htmp, legend_grouping = "original", merge_legends = TRUE, heatmap_legend_side = "top")
+  dev.off()
+  
+  
